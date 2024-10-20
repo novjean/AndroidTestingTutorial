411ac7be13d90367b158e4970edeefa06837d44b
Novjean J Kannathara <novjeanj@gmail.com>
2024-10-20 12:28:07 +0530

    test cases have been defined

--- app/src/test/java/com/novatech/androidtestingtutorial/RegistrationUtilTest.kt
+++ app/src/test/java/com/novatech/androidtestingtutorial/RegistrationUtilTest.kt
@@ -15,4 +15,47 @@ class RegistrationUtilTest{
         assertThat(result).isFalse()
     }
 
+    @Test
+    fun `valid username and correctly repeated password returns true`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Nova", "123", "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl", "123", "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    // password is empty
+    @Test
+    fun `password is empty returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl", "", ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    // password repeated incorrectly
+    @Test
+    fun `password repeated incorrectly returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl", "123", "122"
+        )
+        assertThat(result).isFalse()
+    }
+
+    // password contains less than two digits
+    @Test
+    fun `password contains less than two digits returns false`(){
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl", "abc1", "abc1"
+        )
+        assertThat(result).isFalse()
+    }
+
 }
